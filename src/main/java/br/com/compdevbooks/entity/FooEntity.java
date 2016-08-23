d3afe318ea52ece7344ebcb562826d935145b740
jppreti <jppreti@gmail.com>
2016-08-23 13:52:56 -0400

    Abstract Class for Entity added.

--- src/main/java/br/com/compdevbooks/entity/FooEntity.java
+++ src/main/java/br/com/compdevbooks/entity/FooEntity.java
@@ -1,19 +1,9 @@
 package br.com.compdevbooks.entity;
 
-public class FooEntity implements IEntity {
+public class FooEntity extends AEntity {
 
-    private Long id;
     private String fooString;
     private double fooNumber;
-
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFooString() {
         return fooString;
@@ -29,16 +19,6 @@ public class FooEntity implements IEntity {
 
     public void setFooNumber(double fooNumber) {
         this.fooNumber = fooNumber;
-    }
-
-    @Override
-    public boolean equals(Object entity) {
-        if (this == entity) return true;
-        if (entity == null || getClass() != entity.getClass()) return false;
-
-        FooEntity that = (FooEntity) entity;
-
-        return id.equals(that.id);
     }
 
 }
